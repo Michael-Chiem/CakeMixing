1cd6b870bbd6232c9cb7fa908b9d344d9270f7d3
Michael Chiem <michaelchiem1@hotmail.com>
2024-03-02 16:08:30 -0700

    R1

---
+++ index.js
@@ -0,0 +1,78 @@
+﻿const inquirer = require("inquirer");
+const { Square, Circle, Triangle } = require("./lib/shapes");
+const fs = require("fs");
+
+const initialQuestion = {
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: ['Add Shape', 'Exit'],
+    name: 'initialChoice'
+}
+
+
+const shapeQuestion = [{
+    type: 'list',
+    message: 'What shape would you like to add?',
+    choices: ['Circle', 'Square', 'Triangle'],
+    name: 'shapeChoice'
+},
+{
+    type: 'input',
+    message: 'What color would you like the shape to be?',
+    name: 'shapeColor'
+},
+{
+    type: 'input',
+    message: 'What text would you like the shape to be?',
+    name: 'shapeText'
+},
+{
+    type: 'input',
+    message: 'What color would you like the text to be?',
+    name: 'shapeTextColor'
+}]
+
+
+inquirer.prompt(initialQuestion).then((answer) => {
+    //console.log(answer);
+    if (answer.initialChoice === 'Add Shape') {
+
+        addShape();
+
+    } else { return }
+})
+
+
+function addShape() {
+    inquirer.prompt(shapeQuestion).then((answer) => {
+
+        //console.log(answer);
+        let newShape;
+
+        if (answer.shapeChoice === 'Circle') {
+             newShape = new Circle(answer.shapeColor);
+        } else if (answer.shapeChoice === 'Square') {
+             newShape = new Square(answer.shapeColor);
+        } else {
+             newShape = new Triangle(answer.shapeColor);
+        }
+
+        newShape.setColor(answer.shapeColor);
+        const shapeElement = newShape.render();
+
+        const svgData =
+            `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+${shapeElement}
+
+<text x="150" y="150" font-size="60" text-anchor="middle" fill="${a.shapeTextColor}">${a.shapeText}</text>
+
+</svg>`;
+
+        fs.writeFileSync('logo.svg', svgData);
+
+    })
+
+
+
+}
